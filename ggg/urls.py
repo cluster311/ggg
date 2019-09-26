3b76e34ec4238e48869523387258726a1a6b7bc4
Andrés Vázquez <andres@data99.com.ar>
2019-09-26 19:56:58 -0300

    fix urls

--- ggg/urls.py
+++ ggg/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls import url, include
 urlpatterns = [
     url(r'^jet/', include('jet.urls', 'jet')),
     path('admin/', admin.site.urls),
+    path('accounts/', admin.site.urls),
     url(r'^tinymce/', include('tinymce.urls')),
     path('', admin.site.urls),
     url(r'^profesionales/', include('profesionales.urls')),
