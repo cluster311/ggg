c051eccbf468b318057038f539c792c1bd7efb87
Andrés Vázquez <andres@data99.com.ar>
2019-09-27 15:22:22 -0300

    tableros de obras sociales por provincia

--- ggg/urls.py
+++ ggg/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     url(r'^tinymce/', include('tinymce.urls')),
     path('', admin.site.urls),
     url(r'^profesionales/', include('profesionales.urls')),
+    url(r'^core/', include('core.urls')),
 ]
