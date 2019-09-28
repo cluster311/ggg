0a77bd3734f4b8756ed91f7429a420ff169fda2c
wilmays10 <wilmays10@gmail.com>
2019-09-28 19:48:07 -0300

    fix settings para usar db postgis

--- ggg/settings.py
+++ ggg/settings.py
@@ -84,9 +84,13 @@ WSGI_APPLICATION = 'ggg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
+
 }
 
 """ SI SE CANSAN DE ESTA BASE pueden hacer esto en local_settings
