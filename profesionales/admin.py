01a0edf41397b69e52448ef831d1806f2377409f
Martín Gaitán <gaitan@gmail.com>
2019-09-30 14:58:22 -0300

    redo config admin
    This reverts commit bd9ce5b875c52a3b25ad00e430313284dc4ae3cb.

--- profesionales/admin.py
+++ profesionales/admin.py
@@ -1,19 +1,5 @@
 from django.contrib import admin
 from .models import Profesional
-from .views import calendario
-from django.conf.urls import url
-
-
-
-class ProfesionalSite(admin.AdminSite):
-  
-    def get_urls(self):
-        urls = super(CustomAdminSite, self).get_urls()
-        custom_urls = [
-            url(r'calendario$', self.admin_view(organization_admin.calendario), name="preview"),
-        ]
-        return urls + custom_urls
-
 
 @admin.register(Profesional)
 class ProfesionalAdmin(admin.ModelAdmin):
--- profesionales/urls.py
+++ profesionales/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url, include
 from .views import (TableroProfesionalesPorEspecialidadView,
-                    TableroProfesionalesPorLocalidadView,
-                    calendario)
+                    TableroProfesionalesPorLocalidadView)
 
 
 urlpatterns = [
     url(r'^por-profesion.html$', TableroProfesionalesPorEspecialidadView.as_view(), name='profesionales.tablero.por_profesion'),
 
     url(r'^por-departamento.html$', TableroProfesionalesPorLocalidadView.as_view(), name='profesionales.tablero.por_departamento'),
-    url(r'^calendario$', calendario, name='profesionales.calendario'),
-]
+    
+    ]
