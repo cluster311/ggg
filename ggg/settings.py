a97fa52b5dc5a520a9c849fd8fc659e7020f6de2
Martín Gaitán <gaitan@gmail.com>
2019-09-30 20:37:37 -0300

    back de jet

--- ggg/settings.py
+++ ggg/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # 'jet',
+    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
