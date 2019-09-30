565576ab64baf933bf7027071aeeb7fbd11e31cb
Martín Gaitán <gaitan@gmail.com>
2019-09-30 15:02:00 -0300

    calendario basico

--- profesionales/urls.py
+++ profesionales/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url, include
 from .views import (TableroProfesionalesPorEspecialidadView,
-                    TableroProfesionalesPorLocalidadView)
+                    TableroProfesionalesPorLocalidadView,
+                    calendario)
 
 
 urlpatterns = [
     url(r'^por-profesion.html$', TableroProfesionalesPorEspecialidadView.as_view(), name='profesionales.tablero.por_profesion'),
 
     url(r'^por-departamento.html$', TableroProfesionalesPorLocalidadView.as_view(), name='profesionales.tablero.por_departamento'),
-    
-    ]
+    url(r'^calendario$', calendario, name='profesionales.calendario'),
+]
