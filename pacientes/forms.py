f05db126ee6661912ad846ce62be06f2daabac7d
wilmays10 <wilmays10@gmail.com>
2019-10-16 16:28:25 -0300

    forms update de consulta basado en crispy. Fixed #27

--- pacientes/forms.py
+++ pacientes/forms.py
@@ -2,6 +2,8 @@ from dal import autocomplete
 from django import forms
 from cie10_django.models import CIE10
 from pacientes.models import Consulta, Paciente
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 
 class ConsultaForm(forms.ModelForm):
@@ -27,13 +29,27 @@ class ConsultaForm(forms.ModelForm):
         model = Consulta
         fields = ('__all__')
 
+    def __init__(self, *args, **kwargs):
+        """
+        Form update basado en lib crispy
+        """
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Actualizar'))
+
 
 class PacienteForm(forms.ModelForm):
     numero_documento = forms.ModelChoiceField(
         queryset=Paciente.objects.all(),
-        widget=autocomplete.ModelSelect2(url='paciente-autocomplete')
+        widget=autocomplete.ModelSelect2(url='paciente-autocomplete',
+                                         attrs={
+                                                'data-placeholder': 'Ingrese número de documento',
+                                                'data-minimum-input-length': 3,
+                                        },
+        )
     )
 
     class Meta:
         model = Paciente
-        fields = ('__all__')
+        fields = ('numero_documento', )
