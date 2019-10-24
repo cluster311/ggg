a7e5e68f69ef0bafbd1dd7de697755df447909c6
Andrés Vázquez <andres@data99.com.ar>
2019-10-23 23:09:36 -0300

    redefiniendo la base y excluyendo menues para usuarios no logueados

--- ggg/urls.py
+++ ggg/urls.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
+from .views import HomeView
 
 
 urlpatterns = [
     # url(r'^jet/', include('jet.urls', 'jet')),
+    url(
+        '',
+        HomeView.as_view(),
+        name="admin.home",
+    ),
     path('admin/', admin.site.urls),
     path('accounts/', admin.site.urls),
     url(r'^tinymce/', include('tinymce.urls')),
-    path('', admin.site.urls),
     url(r'^core/', include('core.urls')),
     url(r'^profesionales/', include('profesionales.urls')),
     url(r'^turnos/', include('calendario.urls')),
---
+++ ggg/views.py
@@ -0,0 +1,19 @@
+from django.views.generic import TemplateView
+from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.conf import settings
+
+
+@method_decorator(cache_page(60 * 60 * 24), name='dispatch')
+class HomeView(TemplateView):
+    """ Home del sistema """
+
+    template_name = "home.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["short_title"] = settings.SYS_SHORT_TITLE
+        context["title"] = settings.SYS_TITLE
+
+        return context
