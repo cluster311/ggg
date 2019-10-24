f9affd894a6a1aa83b25205259ca1b1b096092cd
Andrés Vázquez <andres@data99.com.ar>
2019-10-23 23:34:11 -0300

    hotfix url

--- ggg/urls.py
+++ ggg/urls.py
@@ -6,11 +6,6 @@ from .views import HomeView
 
 urlpatterns = [
     # url(r'^jet/', include('jet.urls', 'jet')),
-    url(
-        '',
-        HomeView.as_view(),
-        name="admin.home",
-    ),
     path('admin/', admin.site.urls),
     path('accounts/', admin.site.urls),
     url(r'^tinymce/', include('tinymce.urls')),
@@ -18,4 +13,10 @@ urlpatterns = [
     url(r'^profesionales/', include('profesionales.urls')),
     url(r'^turnos/', include('calendario.urls')),
     url(r'^obras-sociales/', include('obras_sociales.urls')),
+    url(
+        '',
+        HomeView.as_view(),
+        name="admin.home",
+    ),
+    
 ]
