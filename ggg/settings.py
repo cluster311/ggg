df63a295961d4069891d5943313aa578ed75ca09
Adrián Pardini <github@tangopardo.com.ar>
2019-10-29 10:19:12 -0300

    adds allauth templates to our project

--- ggg/settings.py
+++ ggg/settings.py
@@ -82,6 +82,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'ggg', 'templates'),
+            os.path.join(BASE_DIR, 'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
