319900aac14af7010606152ab0abc408feb2fd35
Andrés Vázquez <andres@data99.com.ar>
2019-10-28 21:39:17 -0300

    empezando menues x permisos y listas

--- profesionales/views.py
+++ profesionales/views.py
@@ -25,7 +25,7 @@ class ProfesionalListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['now'] = timezone.now()
+        context['extras'] = []
         return context
 
 
