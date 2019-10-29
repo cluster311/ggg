23bc3bced721a7216a0c75f568295900633c99a9
Andrés Vázquez <andres@data99.com.ar>
2019-10-28 23:14:56 -0300

    lista de centros de salud

--- centros_de_salud/views.py
+++ centros_de_salud/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.db.models import Q
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.conf import settings
+from .models import CentroDeSalud
 
-# Create your views here.
+
+@method_decorator(cache_page(60 * 5), name='dispatch')
+class CentroDeSaludListView(PermissionRequiredMixin, ListView):
+    model = CentroDeSalud
+    permission_required = ("view_centrodesalud",)
+    paginate_by = 10  # pagination
+
+    def get_queryset(self):        
+        if 'search' in self.request.GET:
+            q = self.request.GET['search']
+            objects = CentroDeSalud.objects.filter(
+                Q(nombre__icontains=q) |
+                Q(codigo_hpgd__icontains=q)
+                )
+        else:
+            objects = CentroDeSalud.objects.all()
+        
+        return objects
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['search_txt'] = self.request.GET.get('search', '')
+        return context
