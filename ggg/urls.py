c8d64aabe79ffe71147192efb3bfb5eb2724ec06
Martín Gaitán <gaitan@gmail.com>
2019-11-09 19:20:27 -0300

    implemento una vista de dispatcher dependiendo el tipo de user desde el mismo login

--- ggg/urls.py
+++ ggg/urls.py
@@ -1,23 +1,20 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
-from .views import HomeView
+from .views import LandingPage, choice_homepage
 
 
 urlpatterns = [
     # url(r'^jet/', include('jet.urls', 'jet')),
-    path('admin/', admin.site.urls),
-    url(r'^accounts/', include('allauth.urls')),
-    url(r'^tinymce/', include('tinymce.urls')),
-    url(r'^core/', include('core.urls')),
-    url(r'^profesionales/', include('profesionales.urls')),
-    url(r'^turnos/', include('calendario.urls')),
-    url(r'^obras-sociales/', include('obras_sociales.urls')),
-    url(r'^centros_de_salud/', include('centros_de_salud.urls')),
-    url(r'^recupero/', include('recupero.urls')),
-    url(
-        r'^$',
-        HomeView.as_view(),
-        name="admin.home",
-    ),
+    path("admin/", admin.site.urls),
+    url(r"^accounts/", include("allauth.urls")),
+    url(r"^tinymce/", include("tinymce.urls")),
+    url(r"^core/", include("core.urls")),
+    url(r"^profesionales/", include("profesionales.urls")),
+    url(r"^turnos/", include("calendario.urls")),
+    url(r"^obras-sociales/", include("obras_sociales.urls")),
+    url(r"^centros_de_salud/", include("centros_de_salud.urls")),
+    url(r"^recupero/", include("recupero.urls")),
+    url(r"^$", LandingPage.as_view(), name="landing"),
+    url(r"^home$", choice_homepage, name="home"),
 ]
