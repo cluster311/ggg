a9100845b337cf4d440de3cb01f517aceb3baec4
alan <alan@debian>
2019-11-09 13:05:22 -0300

    logica provisoria para chequear si es municipal

--- ggg/templatetags/extra_tags.py
+++ ggg/templatetags/extra_tags.py
@@ -4,6 +4,8 @@ register = template.Library()
 
 @register.simple_tag
 def es_municipal_admin(user):
-    #TODO: Logica para saber si tiene fucionalidades de admin de municipalidad
-    return False
+    #TODO: Cambiar esta funcionalidad por el chequeo de que este dentro del grupo 'Municipal'
+    return (user.has_perm('profesionales.can_view_tablero') | 
+        user.has_perm('centros_de_salud.can_view_tablero') |
+        user.has_perm('obras_sociales.can_view_tablero'))
 
