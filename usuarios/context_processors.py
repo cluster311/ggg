787056652e1e616536ec0be99f8faa3157bd3c4d
Andres Vazquez <andres@data99.com.ar>
2019-11-10 21:29:04 -0300

    mostrar lista de centros de salud autorizados en la home

--- usuarios/context_processors.py
+++ usuarios/context_processors.py
@@ -10,6 +10,10 @@ def cpp_usuarios(request):
         centros_de_salud_permitidos = user.centros_de_salud_permitidos.all()
 
     context = {}
-    context["centros_de_salud_autorizados"] = centros_de_salud_permitidos
+    context["user__centros_de_salud_autorizados"] = centros_de_salud_permitidos
+
+    context["user__es_ciudadno"] = user.groups.filter(name=settings.GRUPO_CIUDADANO).exists()
+    context["user__es_administrativo"] = user.is_superuser or user.groups.filter(name=settings.GRUPO_ADMIN).exists()
+    context["user__es_profesional"] = user.groups.filter(name=settings.GRUPO_PROFESIONAL).exists()
     
     return context
