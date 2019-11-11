15095314248638e7755dbe535448ba7e43561ae6
Andres Vazquez <andres@data99.com.ar>
2019-11-10 22:36:13 -0300

    fix centros de salud permitidos

--- usuarios/context_processors.py
+++ usuarios/context_processors.py
@@ -10,7 +10,7 @@ def cpp_usuarios(request):
     if user.is_superuser:
         centros_de_salud_permitidos = CentroDeSalud.objects.all()
     else:
-        centros_de_salud_permitidos = user.centros_de_salud_permitidos.all()
+        centros_de_salud_permitidos = [centro.centro_de_salud for centro in user.centros_de_salud_permitidos.all()]
 
     context["user__centros_de_salud_autorizados"] = centros_de_salud_permitidos
     
