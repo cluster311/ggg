0b1f5e2207770599953dcbbe16841a7e82689dee
wilmays10 <wilmays10@gmail.com>
2019-11-16 17:27:01 -0300

    comento el js ya que no se usa por ahora. Fixed #97

--- profesionales/static/js/profesional_home.js
+++ profesionales/static/js/profesional_home.js
@@ -1,28 +1,17 @@
 $( document ).ready(function() {
+    // COMPLETAR PARA QUE UN SELECTOR POR ESTADO DE LOS TURNOS
+    // $("#estado_seleccionado").click(function () {
+    //     var estado_seleccionado = $("#estado_seleccionado :selected").val();
+    //     var url = $("#formulario_estado").data("url");
 
-    $("#estado_seleccionado").click(function () {
-        var estado_seleccionado = $("#estado_seleccionado :selected").val();
-        var url = $("#formulario_estado").data("url");
+    //     $.ajax({ 
+    //         url: url,
+    //         type: "GET",
 
-        $.ajax({ 
-            url: url,
-            type: "GET",
+    //         success: function(response){
+    //             console.log(response);
+    //         }
+    //     });
 
-            success: function(response){
-                console.log(response);
-            }
-            // success: function (data) {
-            //   $('#turnos_filtrados').empty();
-            //   if (data.results.length == 0)
-            //     return;
-
-            //   // $.each(data.results, function( i, profesional ) {
-            //   //   let op = '<option value="'+profesional.id+'">'+profesional.text + '</option>';
-            //   //   $('#id_profesional').append(op);
-            //   // });
-            //   console.log("Borre todo");
-            //   }
-        });
-
-    });
+    // });
 });
