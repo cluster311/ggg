72537b10314c50d8dd8f3d209a8ff475bf10cb70
Martín Gaitán <gaitan@gmail.com>
2019-11-20 21:08:55 -0300

    setear campos de medidas obligatorias en el form de consultas

--- especialidades/forms.py
+++ especialidades/forms.py
@@ -31,8 +31,7 @@ class MedidasAnexasEspecialidadForm(forms.ModelForm):
 
 class MedidaAnexaEnConsultaForm(forms.ModelForm):
 
-    valor = forms.DecimalField(initial=0.0,
-                               widget=forms.NumberInput(attrs={'step': 0.25})) 
+    valor = forms.DecimalField(widget=forms.NumberInput(attrs={'step': 0.25})) 
     
     def __init__(self, *args, **kwargs):
         user = kwargs.pop('user', None)
