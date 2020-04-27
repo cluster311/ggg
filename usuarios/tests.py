af4073a68562852707805ddeb9b5ca2f18cd3028
Marco Richetta <marcorichetta@gmail.com>
2020-04-27 12:46:05 -0300

    Form solo muestra especialidades de CS permitidos

--- usuarios/tests.py
+++ usuarios/tests.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.test import Client, TestCase
 from core.base_permission import start_roles_and_permissions, create_test_data
 from centros_de_salud.models import CentroDeSalud
+from calendario.forms import TurnoForm
 
 
 class FullUserMixin:
@@ -42,3 +43,23 @@ class AdministrativosTest(TestCase, FullUserMixin):
         
         self.assertTrue(len(CS_Permitidos) == 1)
         self.assertEqual('Centro de Salud 1', CS_Permitidos[0].nombre)
+    
+    def test_especialidades_permitidas_form_turno(self):
+        ''' Usuario `administrativo1` solamente puede agregar turnos de 
+            Especialidades en Centros de Salud permitidos '''
+        
+        self.client.login(username=self.user_admin_1, password=self.user_admin_1)
+
+        # Instanciar el formulario de Turnos para nuestro usuario
+        form = TurnoForm(user=self.user_admin_1)
+
+        # Obtener la especialidad (Servicio de un centro de salud) 
+        # permitida para este usuario
+        
+        # El método get() devuelve un error si existe más de 1 objeto
+        servicio = form.fields['servicio'].queryset.get()
+
+        # El usuario administrativo1 solo puede agendar turnos
+        # de especialidades del CS 1 que es el que tiene asignado
+        self.assertEqual(servicio.centro.nombre, 'Centro de Salud 1')
+        self.assertEqual(servicio.especialidad.nombre, 'Especialidad 1')
