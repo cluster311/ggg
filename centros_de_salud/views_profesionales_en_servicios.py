433592a7aedb62305dea20b39376a807d6c6294d
Facundo Ramos <ramos.facundo.m@gmail.com>
2020-06-12 08:32:40 -0300

    creacion de filtro para profesionales en servicio

--- centros_de_salud/views_profesionales_en_servicios.py
+++ centros_de_salud/views_profesionales_en_servicios.py
@@ -1,7 +1,7 @@
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
-from django.db.models import Count, Q
+from django.db.models import Count, Q, F
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse
@@ -21,11 +21,14 @@ class ProfesionalesEnServicioListView(PermissionRequiredMixin, ListView):
     paginate_by = 10
 
     def get_queryset(self):
-
         csp = self.request.user.centros_de_salud_permitidos.all()
         permitidos = [c.centro_de_salud for c in csp]
         qs = ProfesionalesEnServicio.objects.filter(servicio__centro__in=permitidos)
 
+        if 'especialidad' in self.request.GET:
+            q = self.request.GET['especialidad']
+            if not q == '---':
+                qs = qs.filter(servicio__especialidad_id=q)
         if 'search' in self.request.GET:
             q = self.request.GET['search']
             qs = qs.filter(
@@ -43,9 +46,24 @@ class ProfesionalesEnServicioListView(PermissionRequiredMixin, ListView):
         context['title_url'] = 'centros_de_salud.profesionales-en-servicio'
         context['search_txt'] = self.request.GET.get('search', '')
         context['use_search_bar'] = True
+        context['use_filter_bar'] = True
         if self.request.user.has_perm('centros_de_salud.add_profesionalesenservicio'):
             context['use_add_btn'] = True
             context['add_url'] = 'centros_de_salud.profesionales-en-servicio.create'
+        csp = self.request.user.centros_de_salud_permitidos.all()
+        permitidos = [c.centro_de_salud for c in csp]
+        context['especialidad'] = ProfesionalesEnServicio.objects.filter(servicio__centro__in=permitidos).annotate(identificador=F('servicio__especialidad__id'), valor=F('servicio__especialidad__nombre')).values('identificador', 'valor').distinct()
+        filter = []
+        filter_txt = ''
+        if len(context['especialidad']) > 0:
+            get_especialidad = self.request.GET.get('especialidad', '',)
+            if get_especialidad and not get_especialidad == '---':
+                filter_txt += '&especialidad=' + str(get_especialidad)
+                filter.append(('especialidad', 'Especialidad', context['especialidad'], str(get_especialidad)))
+            else:
+                filter.append(('especialidad', 'Especialidad', context['especialidad'], None))
+        context['filters'] = filter
+        context['filter_txt'] = filter_txt
         return context
 
 
