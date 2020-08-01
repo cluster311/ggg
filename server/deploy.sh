e2e7970eca8cf2cc9f1abcde7ed38ea76b5fb24c
Andres Vazquez <andres@data99.com.ar>
2020-08-01 19:01:04 -0300

    imporve deploy

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,9 +1,9 @@
-cd ~
 source ~/env/bin/activate
-cd ggg/
+cd ~/ggg/
 git pull
 pip install -r requirements.txt
 ./manage.py migrate
 ./manage.py collectstatic --no-input
 ./manage.py start_permissions
 sudo supervisorctl restart ggg
+./server/bump_version.sh
