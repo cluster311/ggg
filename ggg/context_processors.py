55f212d7c17ea67ac2126ce329c16fe11d23a295
Andres Vazquez <andres@data99.com.ar>
2020-08-01 16:57:44 -0300

    Agregar numero de version

--- ggg/context_processors.py
+++ ggg/context_processors.py
@@ -6,4 +6,5 @@ def cpp_settings(request):
     context["site_short_title"] = settings.SYS_SHORT_TITLE
     context["site_title"] = settings.SYS_TITLE
     context["site_description"] = settings.SYS_DESCRIPTION
+    context["app_version"] = settings.VERSION
     return context
