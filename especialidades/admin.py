a03344574d52c251884310e78bb42080072c5737
Marco Richetta <marcorichetta@gmail.com>
2020-09-09 22:03:00 -0300

    Apps calendario y especialidades

--- especialidades/admin.py
+++ especialidades/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
-from .models import MedidaAnexaEnConsulta, MedidaAnexa
+from .models import MedidaAnexaEnConsulta, MedidaAnexa, MedidasAnexasEspecialidad
 
 
+@admin.register(MedidaAnexa)
+class MedidaAnexaAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'observaciones_para_el_que_mide']
+
+
+@admin.register(MedidasAnexasEspecialidad)
+class MedidasAnexasEspecialidadAdmin(admin.ModelAdmin):
+    list_display = (
+        'especialidad',
+        'medida',
+        'obligatorio',
+        'observaciones_para_el_que_mide',
+    )
 
 @admin.register(MedidaAnexaEnConsulta)
 class MedidaAnexaEnConsultaAdmin(admin.ModelAdmin):
     list_display = ['consulta', 'medida', 'valor']
-
-
-admin.site.register(MedidaAnexa)
