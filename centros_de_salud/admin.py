1a87b0b05372ee1fd2ad6a8fb29a7a197442078c
Marco Richetta <marcorichetta@gmail.com>
2020-09-09 21:44:25 -0300

    App centros de salud

--- centros_de_salud/admin.py
+++ centros_de_salud/admin.py
@@ -1,20 +1,36 @@
 from django.contrib import admin
-from .models import CentroDeSalud, Servicio, Especialidad, ProfesionalesEnServicio
+from .models import Institucion, CentroDeSalud, Especialidad, Servicio, ProfesionalesEnServicio
 from core.admin import GeoAdmin
 
-
 @admin.register(CentroDeSalud)
-# class CentroDeSaludAdmin(gisadmin.OSMGeoAdmin):
 class CentroDeSaludAdmin(GeoAdmin):
-
-    list_display = ['nombre', 'direccion', 'horario_de_atencion', 'telefonos']
-    # list_display = ['nombre', 'descripcion', 'direccion',
-    #                 'horario_de_atencion', 'telefonos']
+    list_display = (
+        'nombre',
+        'institucion',
+        'codigo_hpgd',
+        'horario_de_atencion',
+        'telefonos',
+    )
     search_fields = ['nombre', 'descripcion']
+
+
+@admin.register(Especialidad)
+class EspecialidadAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'tiempo_predeterminado_turno')
+
+
+@admin.register(Servicio)
+class ServicioAdmin(admin.ModelAdmin):
+    list_display = ('centro', 'especialidad')
+    list_filter = ('centro', 'especialidad')
+    autocomplete_fields = ['centro']
+
 
 @admin.register(ProfesionalesEnServicio)
 class ProfesionalesEnServicioAdmin(admin.ModelAdmin):
-    list_display = ['servicio', 'profesional', 'estado']
+    list_display = ('servicio', 'profesional', 'estado')
+    list_filter = ('servicio', 'profesional')
 
-admin.site.register(Servicio)
-admin.site.register(Especialidad)
+@admin.register(Institucion)
+class InstitucionAdmin(admin.ModelAdmin):
+    list_display = ('nombre',)
