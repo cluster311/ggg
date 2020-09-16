5017e014a46f8180b743d6926ac2a6671f8b92fa
Andres Vazquez <andres@data99.com.ar>
2020-09-16 18:29:13 -0300

    bump version

--- ggg/settings.py
+++ ggg/settings.py
@@ -1,6 +1,6 @@
 import os
 
-VERSION = '1.2.109'
+VERSION = '1.2.111'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
